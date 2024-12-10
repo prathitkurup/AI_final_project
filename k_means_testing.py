e03cf0843a1fe0d03b7f68078c29d0c3992a55db
Prathit Kurup <prathitkurup@gmail.com>
2024-12-10 13:03:11 -0500

    update testing

--- k_means_testing.py
+++ k_means_testing.py
@@ -1,3 +1,5 @@
+# COPIED FROM https://www.geeksforgeeks.org/k-means-clustering-introduction/
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
@@ -25,3 +27,57 @@ for idx in range(k):
     clusters[idx] = cluster
     
 print(clusters)
+
+plt.scatter(X[:,0],X[:,1])
+plt.grid(True)
+for i in clusters:
+    center = clusters[i]['center']
+    plt.scatter(center[0],center[1],marker = '*',c = 'red')
+plt.show()
+
+def distance(p1,p2):
+    return np.sqrt(np.sum((p1-p2)**2))
+
+#Implementing E step 
+def assign_clusters(X, clusters):
+    for idx in range(X.shape[0]):
+        dist = []
+        
+        curr_x = X[idx]
+        
+        for i in range(k):
+            dis = distance(curr_x,clusters[i]['center'])
+            dist.append(dis)
+        curr_cluster = np.argmin(dist)
+        clusters[curr_cluster]['points'].append(curr_x)
+    return clusters
+        
+#Implementing the M-Step
+def update_clusters(X, clusters):
+    for i in range(k):
+        points = np.array(clusters[i]['points'])
+        if points.shape[0] > 0:
+            new_center = points.mean(axis =0)
+            clusters[i]['center'] = new_center
+            
+            clusters[i]['points'] = []
+    return clusters
+
+def pred_cluster(X, clusters):
+    pred = []
+    for i in range(X.shape[0]):
+        dist = []
+        for j in range(k):
+            dist.append(distance(X[i],clusters[j]['center']))
+        pred.append(np.argmin(dist))
+    return pred
+
+clusters = assign_clusters(X,clusters)
+clusters = update_clusters(X,clusters)
+pred = pred_cluster(X,clusters)
+
+plt.scatter(X[:,0],X[:,1],c = pred)
+for i in clusters:
+    center = clusters[i]['center']
+    plt.scatter(center[0],center[1],marker = '^',c = 'red')
+plt.show()
