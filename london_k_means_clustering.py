c61544d022b4227e82572fb703cf195e995ad4da
Stef0618 <slangshur@bowdoin.edu>
2024-12-15 16:17:34 -0500

    updated london_k_means

--- london_k_means_clustering.py
+++ london_k_means_clustering.py
@@ -6,22 +6,26 @@ import folium
 from branca.colormap import LinearColormap 
 from sklearn.cluster import KMeans
 
-def run_k_means(df, features, k):
-    X = df[features]
-    
-    # Step 1: Implement K-Means Clustering
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(X)
-    df['cluster'] = kmeans.predict(X)
+def visualize_plot(df, k):
 
-    # Step 2: Sort clusters by their centroid values
-    centroids = kmeans.cluster_centers_.flatten()  
-    sorted_indices = np.argsort(-centroids)  
+    cmap = plt.get_cmap('viridis', k)  # Get a colormap with k colors
+    colors = [cmap(i) for i in range(k)]  # Extract k colors from the colormap
+    df['color'] = df['cluster'].apply(lambda x: colors[x])
+
+    #for i in range(k):
+    #    print(f"Cluster {i}: {df.loc[df['cluster'] == i].shape}")
     
-    # Step 3: Create a mapping from cluster index to rank
-    cluster_rank = {cluster: rank for rank, cluster in enumerate(sorted_indices)}
-    df['rank'] = df['cluster'].map(cluster_rank)  
-    
+    # Step 5: Visualize clusters over map
+    plt.figure(figsize=(10, 8))
+    plt.scatter(df['longitude'], df['latitude'], c=df['color'], s=10, alpha=0.6, label='Cluster Points')
+    # plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[:, 0], s=100, c='black', marker='^', label='Centers')
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.title(f'K-Means Clustering of House Prices in London (k={k})')
+    plt.legend()
+    # plt.show()
+
+def visualize_map(df, k):
     # Step 4: Assign colors using Viridis colormap
     viridis = LinearColormap(['#440154', '#3B528B', '#21908C', '#5DC863', '#FDE725'], vmin=0, vmax=k-1)
     df['color'] = df['rank'].map(lambda x: viridis(x))
@@ -44,6 +48,26 @@ def run_k_means(df, features, k):
 
     # Step 6: Save map (need to manually open)
     m.save('map.html')
+
+def run_k_means(df, features, k):
+    X = df[features]
+    
+    # Step 1: Implement K-Means Clustering
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X)
+    df['cluster'] = kmeans.predict(X)
+
+    # Step 2: Sort clusters by their centroid values
+    centroids = kmeans.cluster_centers_.flatten()  
+    sorted_indices = np.argsort(-centroids)  
+    
+    # Step 3: Create a mapping from cluster index to rank
+    cluster_rank = {cluster: rank for rank, cluster in enumerate(sorted_indices)}
+    df['rank'] = df['cluster'].map(cluster_rank)  
+    
+    #Step 4: Visualize the Map
+    visualize_map(df, k)
+    visualize_plot(df, k)
     
     return df
 
