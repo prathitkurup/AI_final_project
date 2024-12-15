78ac8b6a70ae4b68f62cd40a9257a79a255c8400
Shibali Mishra <smishra@bowdoin.edu>
2024-12-15 16:46:41 -0500

    more modulation

--- london_k_means_clustering.py
+++ london_k_means_clustering.py
@@ -8,28 +8,25 @@ from sklearn.cluster import KMeans
 
 def visualize_plot(df, k):
 
-    cmap = plt.get_cmap('viridis', k)  # Get a colormap with k colors
-    colors = [cmap(i) for i in range(k)]  # Extract k colors from the colormap
-    df['color'] = df['cluster'].apply(lambda x: colors[x])
-
     #for i in range(k):
     #    print(f"Cluster {i}: {df.loc[df['cluster'] == i].shape}")
     
     # Step 5: Visualize clusters over map
     plt.figure(figsize=(10, 8))
-    plt.scatter(df['longitude'], df['latitude'], c=df['color'], s=10, alpha=0.6, label='Cluster Points')
-    # plt.scatter(kmeans.cluster_centers_[:, 1], kmeans.cluster_centers_[:, 0], s=100, c='black', marker='^', label='Centers')
+    plt.scatter(df['longitude'],
+                 df['latitude'],
+                 c=df['rank'],
+                 cmap='viridis',
+                 s=10, 
+                 alpha=0.6, 
+                 label='Cluster Points')
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.title(f'K-Means Clustering of House Prices in London (k={k})')
     plt.legend()
-    # plt.show()
+    plt.show()
 
 def visualize_map(df, k):
-    # Step 4: Assign colors using Viridis colormap
-    viridis = LinearColormap(['#440154', '#3B528B', '#21908C', '#5DC863', '#FDE725'], vmin=0, vmax=k-1)
-    df['color'] = df['rank'].map(lambda x: viridis(x))
-
 
     # Step 5: Create a folium map
     m = folium.Map(location=[51.5014, -0.140634], zoom_start=10)
@@ -39,9 +36,9 @@ def visualize_map(df, k):
             folium.CircleMarker(
                 location=[row['latitude'], row['longitude']],
                 radius=5,
-                color=row['color'],  # Outline color
+                color=row['color_map'],  # Outline color
                 fill=True,
-                fill_color=row['color'],  # Fill color
+                fill_color=row['color_map'],  # Fill color
                 fill_opacity=0.3,  # Set fill opacity (30% transparent)
                 opacity=0.3  # Set outline opacity (30% transparent)
             ).add_to(m)
@@ -64,11 +61,14 @@ def run_k_means(df, features, k):
     # Step 3: Create a mapping from cluster index to rank
     cluster_rank = {cluster: rank for rank, cluster in enumerate(sorted_indices)}
     df['rank'] = df['cluster'].map(cluster_rank)  
-    
-    #Step 4: Visualize the Map
-    visualize_map(df, k)
-    visualize_plot(df, k)
-    
+
+    cmap = plt.get_cmap('viridis', k)  # Get a colormap with k colors
+    colors = [cmap(i) for i in range(k)]  # Extract k colors from the colormap
+    df['color_plot'] = df['cluster'].apply(lambda x: colors[x])
+
+    viridis = LinearColormap(['#440154', '#3B528B', '#21908C', '#5DC863', '#FDE725'], vmin=0, vmax=k-1)
+    df['color_map'] = df['rank'].map(lambda x: viridis(x))
+
     return df
 
 if __name__ == "__main__":
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     df = pd.read_csv('cleaned_london_house_price_data.csv')
     
     # Limit data for faster computation
-    # df = df.iloc[:100]
+    # df = df.iloc[:1000]
     
     # Calculate price per square meter and drop unnecessary columns
     df['price_per_sqm'] = df['history_price'] / df['floorAreaSqM']
@@ -88,3 +88,6 @@ if __name__ == "__main__":
     
     # Run K-means clustering and visualize the results on a map
     clustered_df = run_k_means(df, features, 8)
+
+    visualize_map(clustered_df, 8)
+    visualize_plot(clustered_df, 8)
